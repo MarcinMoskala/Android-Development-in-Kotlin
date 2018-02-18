cd1ed5be2ca36e565831e2c65d36e4f50306c662
Marcin Moskala <marcinmoskala@gmail.com>
2018-02-18 08:32:56 +0100

    Explain ConstraintLayout

--- app/src/main/java/com/kotlinacademy/todoapp/TasksActivity.kt
+++ app/src/main/java/com/kotlinacademy/todoapp/TasksActivity.kt
@@ -2,11 +2,13 @@ package com.kotlinacademy.todoapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 class TasksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tasks)
+        Toast.makeText(this, "Hello, I am toast", Toast.LENGTH_LONG).show()
     }
 }
