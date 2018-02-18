52c26983421f2dc27547444fda375779c3fff1fd
Marcin Moskala <marcinmoskala@gmail.com>
2018-02-18 11:41:30 +0100

    Add FAB

--- app/src/main/java/com/kotlinacademy/todoapp/TasksActivity.kt
+++ app/src/main/java/com/kotlinacademy/todoapp/TasksActivity.kt
@@ -3,12 +3,15 @@ package com.kotlinacademy.todoapp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_tasks.*
 
 class TasksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tasks)
-        Toast.makeText(this, "Hello, I am toast", Toast.LENGTH_LONG).show()
+        fab.setOnClickListener {
+            Toast.makeText(this, "Hello, I am toast", Toast.LENGTH_LONG).show()
+        }
     }
 }
