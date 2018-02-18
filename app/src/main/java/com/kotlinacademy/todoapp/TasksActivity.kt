b7a4052a605b677e6f96637af61f34cde5aaefb9
Marcin Moskala <marcinmoskala@gmail.com>
2018-02-18 04:01:58 +0100

    Make empty project

---
+++ app/src/main/java/com/kotlinacademy/todoapp/TasksActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlinacademy.todoapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class TasksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tasks)
+    }
+}
