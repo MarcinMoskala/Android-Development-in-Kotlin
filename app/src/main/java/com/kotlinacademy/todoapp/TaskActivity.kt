5bf972e11ec786798c0be4d55fd6d0e39f08af11
Marcin Moskala <marcinmoskala@gmail.com>
2018-02-27 08:29:56 +0100

    Create tasks

--- app/src/main/java/com/kotlinacademy/todoapp/TaskActivity.kt
+++ app/src/main/java/com/kotlinacademy/todoapp/TaskActivity.kt
@@ -1,12 +1,27 @@
 package com.kotlinacademy.todoapp
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_task.*
 
 class TaskActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task)
+
+        cancelButton.setOnClickListener {
+            setResult(Activity.RESULT_CANCELED)
+            finish()
+        }
+        addTaskButton.setOnClickListener {
+            val name = taskNameView.text.toString()
+            val bundle = Intent()
+            bundle.putExtra("name", name)
+            setResult(Activity.RESULT_OK, bundle)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/kotlinacademy/todoapp/TasksActivity.kt
+++ app/src/main/java/com/kotlinacademy/todoapp/TasksActivity.kt
@@ -1,20 +1,38 @@
 package com.kotlinacademy.todoapp
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_tasks.*
+import kotlin.properties.Delegates.observable
 
 class TasksActivity : AppCompatActivity() {
+
+    var tasks by observable(listOf("Wash dishes", "Make Kotlin course")) { _, _, _ ->
+        refreshList()
+    }
+    val ADD_TASK_REQUEST = 12342
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tasks)
         fab.setOnClickListener {
-            startActivity(Intent(this, TaskActivity::class.java))
+            startActivityForResult(Intent(this, TaskActivity::class.java), ADD_TASK_REQUEST)
         }
-        val list = (1..100).map { "Task number $it" }
-        tasksListView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        refreshList()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == ADD_TASK_REQUEST && resultCode == Activity.RESULT_OK) {
+            val name = data?.getStringExtra("name") ?: return
+            tasks += name
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun refreshList() {
+        tasksListView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, tasks)
     }
 }
